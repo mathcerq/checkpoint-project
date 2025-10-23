682dfb078155516900caf1dd9f178563c0dbdfe0
mathcerq <matt.scerqueira@gmail.com>
2025-10-23 16:14:32 -0300

    fix: Isola configuracao de teste em src/test/resources para que o CI passe

--- src/main/java/com/br/mathcerq/checkpoint_project/CheckpointProjectApplicationTests.java
+++ src/main/java/com/br/mathcerq/checkpoint_project/CheckpointProjectApplicationTests.java
@@ -1,17 +1,15 @@
-package com.br.mathcerq.checkpoint_project; // Use o nome exato do seu pacote!
+package com.br.mathcerq.checkpoint_project;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.junit.jupiter.api.Test;
 
-// As anotações ativam o perfil 'test', que usa o H2 e ignora o PostgreSQL
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test") 
+// Removemos @ActiveProfiles para deixar o Spring carregar o arquivo em src/test/resources
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT) 
 class CheckpointProjectApplicationTests {
 
     @Test
     void contextLoads() {
-        // Este é um teste básico que apenas verifica se a aplicação consegue iniciar.
+        // Teste de contexto
     }
 }
